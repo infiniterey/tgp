f2e7db77864a6e08579ffb07c5233b4c383dbf2c
John <glian.panugaling@gmail.com>
2017-01-05 17:31:53 +0800

    added dbwrapper

--- base/header.php
+++ base/header.php
@@ -2,22 +2,27 @@
 <html>
 <head>
   <title>Login</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="/assets/css/jquery.dataTables.min.css">
 </head>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/assets/js/jquery.dataTables.min.js"></script>
 
 <style type="text/css">
 	.navbar-brand>img {
 	    display: initial;
 	}
+	.navbar-brand{
+		float: none
+	}
 </style>
 
 <body>
 
-<nav class="navbar navbar-default">
+
+
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -26,9 +31,10 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">
-      	<img src="images/tgplogo.png" width="20"> The Great Provider
+      <a class="navbar-brand brand-name" href="#">
+      	<img src="images/tgplogo.png" width="50"> The Great Provider
       </a>
+
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
